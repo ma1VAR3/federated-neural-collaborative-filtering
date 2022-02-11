71f97e7154816963a40ff5a96d73ac8cd1e42471
ma1VAR3 <prajjwalgupta1465@gmail.com>
2022-02-12 04:43:07 +0530

    update

--- server.py
+++ server.py
@@ -1,6 +1,19 @@
 import configparser
+from recommenders.datasets import movielens
 
+def distribute_client_data(n_clients, ml_datasize):
+    sample_frac = 1 / n_clients
+    df = movielens.load_pandas_df(
+        size=ml_datasize,
+        header=["userID", "itemID", "rating", "timestamp"]
+    )
+    client_datas = []
+    for c in range(n_clients):
+        client_datas.append(df.sample(frac=sample_frac))
+    return client_datas
 
+def train_server():
+    pass
 
 
 if __name__=="__main__":
@@ -10,4 +23,6 @@ if __name__=="__main__":
     epochs = int(config['DEFAULT']['EPOCHS'])
     batch_size = int(config['DEFAULT']['BATCH_SIZE'])
     seed = int(config['DEFAULT']['SEED'])
+    n_clients = int(config['DEFAULT']['N_CLIENTS'])
+    client_datas = distribute_client_data(n_clients)
     
